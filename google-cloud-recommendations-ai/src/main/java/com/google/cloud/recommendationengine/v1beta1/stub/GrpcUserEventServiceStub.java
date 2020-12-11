310e86476768986396fdf78d48165c99b751a517
yoshi-automation <yoshi-automation@google.com>
2020-12-11 14:37:16 -0800

    chore: update gapic-generator-java to 0.0.9
    Committer: @miraleung
    PiperOrigin-RevId: 346372957
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Tue Dec 8 11:37:30 2020 -0800
    Source-Repo: googleapis/googleapis
    Source-Sha: 857f70daef9f4c38f042fb3dfb7b8423ae18fd19
    Source-Link: https://github.com/googleapis/googleapis/commit/857f70daef9f4c38f042fb3dfb7b8423ae18fd19

--- google-cloud-recommendations-ai/src/main/java/com/google/cloud/recommendationengine/v1beta1/stub/GrpcUserEventServiceStub.java
+++ google-cloud-recommendations-ai/src/main/java/com/google/cloud/recommendationengine/v1beta1/stub/GrpcUserEventServiceStub.java
@@ -146,11 +146,21 @@ public class GrpcUserEventServiceStub extends UserEventServiceStub {
         UserEventServiceStubSettings.newBuilder().build(), clientContext, callableFactory);
   }
 
+  /**
+   * Constructs an instance of GrpcUserEventServiceStub, using the given settings. This is protected
+   * so that it is easy to make a subclass, but otherwise, the static factory methods should be
+   * preferred.
+   */
   protected GrpcUserEventServiceStub(
       UserEventServiceStubSettings settings, ClientContext clientContext) throws IOException {
     this(settings, clientContext, new GrpcUserEventServiceCallableFactory());
   }
 
+  /**
+   * Constructs an instance of GrpcUserEventServiceStub, using the given settings. This is protected
+   * so that it is easy to make a subclass, but otherwise, the static factory methods should be
+   * preferred.
+   */
   protected GrpcUserEventServiceStub(
       UserEventServiceStubSettings settings,
       ClientContext clientContext,
